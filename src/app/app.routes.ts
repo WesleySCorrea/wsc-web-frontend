e79f29beb6769b9f4401bf0de53d88b312d9ac36
Wesley <Wesleyscorrea@hotmail.com>
2025-02-22 15:19:53 -0300

    Adicionado navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CarsComponent } from './cars/cars.component';
+import { EnterprisesComponent } from './enterprises/enterprises.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent },
     { path: 'login', pathMatch: 'full', component: LoginComponent },
+    { path: 'cars', pathMatch: 'full', component: CarsComponent },
+    { path: 'enterprises', pathMatch: 'full', component: EnterprisesComponent },
+    { path: 'tasks', pathMatch: 'full', component: TasksComponent },
 ];
---
+++ src/app/enterprises/enterprises.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enterprises',
+  imports: [],
+  templateUrl: './enterprises.component.html',
+  styleUrl: './enterprises.component.scss'
+})
+export class EnterprisesComponent {
+
+}
